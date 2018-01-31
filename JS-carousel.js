d95134d317e7b0deb8c4a6289cb445136a370f29
Kda8000 <34623076+Kda8000@users.noreply.github.com>
2018-01-31 15:45:56 +0300

    Add files via upload

--- JS-carousel.js
+++ JS-carousel.js
@@ -1,39 +1,34 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides1");
-    var dots = document.getElementsByClassName("dot1");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000);
+function createSlides(slidesClass, dotsClass){
+    var slideIndex = 0;
+    return function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName(slidesClass);
+        var dots = document.getElementsByClassName(dotsClass);
+        for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2000);
 }
 
-var slideIndex2 = 0;
-showSlides2();
-
-function showSlides2() {
-    var z;
-    var slides2 = document.getElementsByClassName("mySlides2");
-    var dots2 = document.getElementsByClassName("dot2");
-    for (z = 0; z < slides2.length; z++) {
-       slides2[z].style.display = "none";
-    }
-    slideIndex2++;
-    if (slideIndex2 > slides2.length) {slideIndex2 = 1}
-    for (z = 0; z < dot2s.length; z++) {
-        dots2[z].className = dots2[z].className.replace(" active", "");
-    }
-    slides2[slideIndex2-1].style.display = "block";
-    dots2[slideIndex2-1].className += " active";
-    setTimeout(showSlides2, 2000);
 }
+
+var slides1 = createSlides("mySlides1", "dot1");
+var slides2 = createSlides("mySlides2", "dot2");
+var slides3 = createSlides("mySlides3", "dot3");
+var slides4 = createSlides("mySlides4", "dot4");
+var slides5 = createSlides("mySlides5", "dot5");
+var slides6 = createSlides("mySlides6", "dot6");
+slides1();
+slides2();
+slides3();
+slides4();
+slides5();
+slides6();
+
